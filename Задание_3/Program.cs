318f0946ce940ac415e81af1d874efc0f4c963ba
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-12 00:01:02 +0300

    Добавьте файлы проекта.

---
+++ Задание_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 5, 7 };
+            int target = 5;
+            int i;
+
+            for (i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    break;
+                }
+                else if (nums[i] > 5)
+                {
+                    break;
+                }
+            }
+
+
+
+            Console.WriteLine("[" + i + "]");
+        }
+    }
+}
